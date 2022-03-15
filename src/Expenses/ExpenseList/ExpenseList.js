acd5ec77915bf5249ac8356c174208e6ed0fb763
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-15 09:46:32 +0530

    iterating an array of object to generate table

--- src/Expenses/ExpenseList/ExpenseList.js
+++ src/Expenses/ExpenseList/ExpenseList.js
@@ -2,7 +2,32 @@ import { useContext } from "react";
 import { ExpensesContext, UserContext } from "../../App";
 
 export function ExpenseList() {
-	const expense = useContext(ExpensesContext);
-	const user = useContext(UserContext);
-	return <div>{JSON.stringify(expense)}</div>;
+	const expenses = useContext(ExpensesContext);
+
+	return (
+		<>
+			<table>
+				<thead>
+					<tr>
+						<td>Sr No</td>
+						<td>Expense Date</td>
+						<td>Expense Name</td>
+						<td>Amount</td>
+						<td>Category</td>
+					</tr>
+				</thead>
+				<tbody>
+					{expenses.map((exp, idx) => (
+						<tr key={idx}>
+							<td>{idx + 1}</td>
+							<td>{exp.expDate}</td>
+							<td>{exp.title}</td>
+							<td>{exp.amount}</td>
+							<td>{exp.category}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
+	);
 }
